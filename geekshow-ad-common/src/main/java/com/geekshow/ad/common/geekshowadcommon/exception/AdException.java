9e0fa9a7f1e4a1a63df39fe41a4e05e36d0d473e
darth-xiaozhijun <15717061273@163.com>
2019-08-17 18:56:11 +0800

    通用代码模块设计

---
+++ geekshow-ad-common/src/main/java/com/geekshow/ad/common/geekshowadcommon/exception/AdException.java
@@ -0,0 +1,13 @@
+package com.geekshow.ad.common.geekshowadcommon.exception;
+
+/**
+ * AdException for 统一异常处理类
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang</a>
+ */
+public class AdException extends Exception {
+
+    public AdException(String message) {
+        super(message);
+    }
+}
---
+++ geekshow-ad-common/src/main/java/com/geekshow/ad/common/geekshowadcommon/vo/CommonResponse.java
@@ -0,0 +1,38 @@
+package com.geekshow.ad.common.geekshowadcommon.vo;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ *@Data是下属注解的组合注解
+ *
+ * @see Getter
+ * @see Setter
+ * @see RequiredArgsConstructor
+ * @see ToString
+ * @see EqualsAndHashCode
+ * @see lombok.Value
+ */
+@Data
+@NoArgsConstructor//无参构造函数
+@AllArgsConstructor //全参构造函数
+public class CommonResponse<T> implements Serializable {
+
+    private Integer code = 0;
+
+    private String message = "success";
+    /**
+     * 具体的数据对象信息
+     */
+    private T data;
+
+    public CommonResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public CommonResponse(T data) {
+        this.data = data;
+    }
+}
